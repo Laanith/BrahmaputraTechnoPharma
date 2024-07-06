c37b3eaab665ced15c772e9c69124805ed0d92c6
Laanith <lanithchouhan2004@gmail.com>
2024-07-06 15:02:13 +0530

    Add several important pages

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -25,7 +25,7 @@ const Contact = () => {
           <p className="font-bold my-2 border-b-2 border-primary w-[40%] mx-auto">Address</p>
           <p className="px-20">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type{" "}
           </p>
@@ -42,7 +42,7 @@ const Contact = () => {
           <p className="font-bold my-2 border-b-2 border-primary w-[40%] mx-auto ">Phone</p>
           <p className="px-20">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type{" "}
           </p>
@@ -59,7 +59,7 @@ const Contact = () => {
           <p className="font-bold my-2 border-b-2 border-primary w-[40%] mx-auto">Mail</p>{" "}
           <p className="px-20">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type{" "}
           </p>
