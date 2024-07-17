9273a9c2c5ade780b1460c5e0f6493db3e3deeb6
Laanith <lanithchouhan2004@gmail.com>
2024-07-17 12:12:12 +0530

    Final draft

--- src/app/careers/page.js
+++ src/app/careers/page.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 const Careers = () => {
-    return ( <main>Careers</main> );
+    return ( <main className="flex flex-row text-base p-8">
+        <span className="mx-auto">
+            If you are interested in working at Brahmaputra TechnoPharmaceuticals Pvt. Ltd. contact us {" "}<Link href={"https://forms.gle/puwPBfCYdvEuBvjS9"} className="text-blue-600 underline">
+            here
+            </Link>
+        </span>
+    </main> );
 }
  
 export default Careers;
